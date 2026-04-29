c0e26130edf71765b6915361a64a2b0074841567
rem <ceevp0@gmail.com>
2026-04-29 00:42:11 -0400

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,6 @@
+    <!-- NAVIGATION BAR -->
+    <div class="navstrip" style="border-left: 3px solid lightgrey; border-right: 3px solid lightgrey;">
+      <a href="/#.html">PERSONAL</a>
+      <a href="/#.html">CORTIS</a>
+      <a href="/#.html">REALITY</a>
+    </div>
