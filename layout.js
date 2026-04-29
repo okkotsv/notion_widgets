9f013fc7ebbda1aa28f00f321624542121c96179
rem <ceevp0@gmail.com>
2026-04-29 00:44:42 -0400

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,5 +1,4 @@
-    <!-- NAVIGATION BAR -->
-    <div class="navstrip" style="border-left: 3px solid lightgrey; border-right: 3px solid lightgrey;">
+      <div class="navstrip" style="border-left: 3px solid lightgrey; border-right: 3px solid lightgrey;">
       <a href="/#.html">PERSONAL</a>
       <a href="/#.html">CORTIS</a>
       <a href="/#.html">REALITY</a>
